887031ef775c438e1d60f73cb1e3189deb3ef2b1
saddamhossain <saddamhossain953@gmail.com>
2024-10-22 00:38:22 +0600

    Add project files.

---
+++ ClassObjectDemo04/LibraryBook.cs
@@ -0,0 +1,46 @@
+﻿namespace ClassObjectDemo04;
+
+public class LibraryBook
+{
+    public string Title { get; private set; }
+
+    public string Author { get; private set; }
+
+    public int TotalCopies { get; private set; }
+
+    public int AvailableCopies { get; private set; }
+
+    public LibraryBook(string title, string author, int totalCopies)
+    {
+        Title = title;
+        Author = author;
+        TotalCopies = totalCopies;
+        AvailableCopies = totalCopies;
+    }
+
+    public bool BorrowBook()
+    {
+        if (AvailableCopies > 0)
+        {
+            AvailableCopies--;
+            return true;
+        }
+        return false;
+    }
+
+    public bool ReturnBook()
+    {
+        if (AvailableCopies < TotalCopies)
+        {
+            AvailableCopies++;
+            return true;
+        }
+        return false;
+    }
+
+    public bool IsAvailable()
+    {
+        return AvailableCopies > 0;
+    }
+
+}
---
+++ ClassObjectDemo04/Program.cs
@@ -0,0 +1,35 @@
+﻿LibraryBook libraryBook = new LibraryBook("Introduction to Algorithms", "Thomas H. Cormen", 3);
+
+if (libraryBook.BorrowBook())
+{
+    Console.WriteLine($"You have borrowed '{libraryBook.Title}' by {libraryBook.Author}. Copies left: {libraryBook.AvailableCopies}");
+}
+else
+{
+    Console.WriteLine("Sorry, no copies available for borrowing.");
+}
+
+
+if (libraryBook.BorrowBook())
+{
+    Console.WriteLine($"You have borrowed '{libraryBook.Title}' by {libraryBook.Author}. Copies left: {libraryBook.AvailableCopies}");
+}
+else
+{
+    Console.WriteLine("Sorry, no copies available for borrowing.");
+}
+
+if (libraryBook.ReturnBook())
+{
+    Console.WriteLine($"You have returned '{libraryBook.Title}'. Copies available now: {libraryBook.AvailableCopies}");
+}
+
+
+if (libraryBook.IsAvailable())
+{
+    Console.WriteLine($"'{libraryBook.Title}' is available for borrowing.");
+}
+else
+{
+    Console.WriteLine($"'{libraryBook.Title}' is not available for borrowing.");
+}
